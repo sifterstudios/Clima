5a2bb085502a4267feaa0970d06818db4b10a5e9
Tormod Liseth <tormod@sifterstudios.no>
2022-08-11 23:09:59 +0200

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error('Location Not Available');
+      }
+    } else {
+      throw Exception('Error');
+    }
+    Position position = await Geolocator.getCurrentPosition();
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            //Get the current location
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
